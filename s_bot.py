7cbbf23d9d176bce8533f464aa6f3d6eaeeb0445
Sherlock-FH <princefuwad@gmail.com>
2021-09-29 17:46:22 +0600

    logging added

--- s_bot.py
+++ s_bot.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 import logging
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
 handler = logging.FileHandler(
     filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter(
