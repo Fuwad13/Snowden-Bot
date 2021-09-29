2e41f061d50560499ac9e9ad421b23ffc169b48e
Sherlock-FH <princefuwad@gmail.com>
2021-09-29 17:37:43 +0600

    logging added

--- s_bot.py
+++ s_bot.py
@@ -10,6 +10,17 @@ from itertools import cycle
 from discord.ext.commands import BucketType
 from dotenv import load_dotenv
 
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+
 load_dotenv()  # take environment variables from .env.
 
 # --------tokens and keys-----------------
