22f756da24cb5a3668b737f2a864cb5418d26ac1
Sherlock-FH <princefuwad@gmail.com>
2021-10-02 20:43:11 +0600

    snowflake command

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,10 +2,17 @@ import discord
 from discord.ext import commands
 
 
-class Misc(commands.Cog):
+class Helpful(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+	@commands.command(name = 'snowflake', aliases = ["sf", "sfl", "sflake"], brief = "Shows the snowflake creation time!", help = "This command gives you the discord snowflake creation time in a timestamp format.")
+	async def snow_flake(self, ctx, snowflake_id: discord.abc.Snowflake):
+		time = int(discord.utils.snowflake_time(snowflake_id).timestamp())
+		await ctx.send(f"<t:{time}:F>")
+
+	
+
 
 def setup(bot):
-	bot.add_cog(Misc(bot))
+	bot.add_cog(Helpful(bot))
