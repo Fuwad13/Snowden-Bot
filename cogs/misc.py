f29e167d2199e8804c5201455380373ba09f6bd6
Sherlock-FH <princefuwad@gmail.com>
2021-10-02 22:39:24 +0600

    heroku psql check

--- cogs/misc.py
+++ cogs/misc.py
@@ -6,10 +6,7 @@ class Helpful(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(name = 'snowflake', aliases = ["sf", "sfl", "sflake"], brief = "Shows the snowflake creation time!", help = "This command gives you the discord snowflake creation time in a timestamp format.")
-	async def snow_flake(self, ctx, snowflake_id: discord.abc.Snowflake):
-		time = int(discord.utils.snowflake_time(snowflake_id).timestamp())
-		await ctx.send(f"<t:{time}:F>")
+	
 
 	
 
