d5ae25e9ebe03c404e74416087a74d61098408a3
Sherlock-FH <princefuwad@gmail.com>
2021-10-05 20:45:30 +0600

    slash command removed

--- cogs/info.py
+++ cogs/info.py
@@ -10,6 +10,18 @@ class Information(commands.Cog):
 	async def ping(self, ctx):
 		await ctx.send(f"PONG!! {round(self.bot.latency*1000)} ms")
 
+	@commands.command()
+	async def avatar(self, ctx, *, user: Union[discord.Member, discord.User] = None):
+		"""Shows a user's enlarged avatar (if possible)."""
+		embed = discord.Embed()
+		user = user or ctx.author
+		avatar = user.display_avatar.with_static_format('png')
+		embed.set_author(name=str(user), url=avatar)
+		embed.set_image(url=avatar)
+		await ctx.send(embed=embed)
+
+	
+
 
 def setup(bot):
 	bot.add_cog(Information(bot))
