8c4bf1ac8da87e33cd87133c3b46bbd4cb217772
Sherlock-FH <princefuwad@gmail.com>
2021-10-08 04:36:19 +0600

    asyncpg tessss select

--- cogs/games.py
+++ cogs/games.py
@@ -23,7 +23,7 @@ class Games(commands.Cog):
 		if not user:
 			user = ctx.author
 
-		data = await self.bot.db.fetchval(""" SELECT * FROM  user_accounts WHERE id = $1  ;""", ctx.author.id)
+		data = await self.bot.db.fetchval(""" SELECT (id, created_at, snowflake_bal) FROM  user_accounts WHERE id = $1  ;""", ctx.author.id)
 		await ctx.send(f"{data}")
 
 
