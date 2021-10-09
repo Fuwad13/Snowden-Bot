8976f2db7d2bdaa63bb482b5a689cc074138c55d
Sherlock-FH <princefuwad@gmail.com>
2021-10-10 04:03:17 +0600

    added levelling

--- cogs/games.py
+++ cogs/games.py
@@ -5,11 +5,14 @@ import asyncpg
 import typing
 import random
 from utils import buttons_and_selects as bs
+from games_utils import constants as cs 
 
 
 class Games(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+
+	# helper/utility functions
 
 	async def check_if_exists(self, user_id):
 		flag = False
@@ -22,15 +25,35 @@ class Games(commands.Cog):
 
 	async def update_exp(self, *,player_id :int, amount, add :bool = True ):
 		if add == True:
-			bal = await self.bot.db.fetchval(""" SELECT exp FROM snowden_bg WHERE id = $1 ;""", player_id)
-			bal = int(bal) + amount
-			await self.bot.db.execute(""" UPDATE snowden_bg SET exp = $1 WHERE id = $2; """, bal, player_id)
+			c_exp = await self.bot.db.fetchval(""" SELECT exp FROM snowden_bg WHERE id = $1 ;""", player_id)
+			n_exp = int(c_exp) + amount
+			await self.bot.db.execute(""" UPDATE snowden_bg SET exp = $1 WHERE id = $2; """, n_exp, player_id)
 
 		elif add == False:
-			bal = await self.bot.db.fetchval(""" SELECT exp FROM snowden_bg WHERE id = $1 ;""", player_id)
-			bal = int(bal) - amount
-			await self.bot.db.execute(""" UPDATE snowden_bg SET exp = $1 WHERE id = $2; """, bal, player_id)
-		return bal
+			c_exp = await self.bot.db.fetchval(""" SELECT exp FROM snowden_bg WHERE id = $1 ;""", player_id)
+			n_exp = int(c_exp) - amount
+			await self.bot.db.execute(""" UPDATE snowden_bg SET exp = $1 WHERE id = $2; """, n_exp, player_id)
+		return c_exp, n_exp
+
+	def get_level(self, exp : int):
+		level = 0
+		for l, e in cs.EXP_LEVELS.items():
+
+			if exp >= e:
+				level +=1
+				continue
+			else:
+				return level
+
+	def level_up_check(self, c_exp: int, n_exp: int):
+		c_lvl = self.get_level(c_exp)
+		n_lvl = self.get_level(n_exp)
+		if n_lvl>c_lvl:
+			return True
+		return False
+
+	
+
 		
 
 
@@ -52,7 +75,9 @@ class Games(commands.Cog):
 		data = await self.bot.db.fetchrow(""" SELECT * FROM snowden_bg where id = $1;  """, player_id)
 		return data
 
-	
+
+	# commands....
+
 	@commands.command(name = 'profile')
 	async def _profile(self, ctx, player : typing.Union[discord.User, discord.Member] = None):
 		if not player:
@@ -65,7 +90,7 @@ class Games(commands.Cog):
 		if not flag:
 			return await ctx.send(f"{player} don't have any account yet, to create one , run the `start` command first!")
 		data = await self.get_user_inventory(player_id)
-		embed = discord.Embed(title = f"{player}'s Profile:", description = f"Balance : **${float(data['balance'])}**\nAccount created: <t:{data['created_at']}:f>\nExperience points : <:exp:896086434946097162> {data['exp']} exp")
+		embed = discord.Embed(title = f"{player}'s Profile:", description = f"\U0001f3e6 Balance : **${float(data['balance'])}**\n<:exp:896086434946097162> Experience points : **{data['exp']}** EXP\n\U0001f4c8 Level : **{self.get_level(data['exp'])}**\nAccount created: <t:{data['created_at']}:f>")
 		await ctx.send(embed = embed)
 	
 
@@ -121,21 +146,32 @@ class Games(commands.Cog):
 			won_or_lost = random.randint(0,1)
 			if won_or_lost == 1:
 				g_exp = 50
-				exp = await self.update_exp(player_id = ctx.author.id,amount= 50)
+				c_exp, n_exp = await self.update_exp(player_id = ctx.author.id,amount= 50)
 				bal = await self.update_balance(player_id = ctx.author.id,amount =  amount, add = True)
 				
-				text = f"\U0001f38a **Congrats** {ctx.author.name},\nThe coin landed on **Heads!** You chose **Heads**, meaning that you've just won **${amount}**!! \n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp**  "
+				c_lvl = self.get_level(c_exp)
+				lvl_up = self.level_up_check(c_exp, n_exp)
+
+				text = f"\U0001f38a **Congrats** {ctx.author.name},\nThe coin landed on **Heads!** You chose **Heads**, meaning that you've just won **${amount}**!! \n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp from this coinflip**"
+				if lvl_up:
+					text+=f"\n\U0001f389 You levelled up! `({c_lvl} -> {c_lvl+1})` and gained **$1000**"
+					await self.update_balance(player_id = ctx.author.id,amount =  1000, add = True)
 				embed.description = text
 				await msg.edit(embed = embed , view = view)
 
 			elif won_or_lost == 0:
 				g_exp = 25
-				exp = await self.update_exp(player_id = ctx.author.id,amount= 25)
+				c_exp, n_exp = await self.update_exp(player_id = ctx.author.id,amount= 25)
 
+				c_lvl = self.get_level(c_exp)
+				lvl_up = self.level_up_check(c_exp, n_exp)
 
 				bal = await self.update_balance(player_id = ctx.author.id, amount = amount,add = False)
 				
-				text = f"\U0001f626 **Aw snap,**{ctx.author.name},\nThe coin landed on **Tails** You chose **Heads**, meaning that you've just lost **${amount}**!\n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp** "
+				text = f"\U0001f626 **Aw snap,**{ctx.author.name},\nThe coin landed on **Tails** You chose **Heads**, meaning that you've just lost **${amount}**!\n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp from this coinflip** "
+				if lvl_up:
+					text+=f"\n\U0001f389 You levelled up! `({c_lvl} -> {c_lvl+1})` and gained **$1000**"
+					await self.update_balance(player_id = ctx.author.id,amount =  1000, add = True)
 				embed.description = text
 				await msg.edit(embed = embed , view = view)
 		
@@ -143,19 +179,32 @@ class Games(commands.Cog):
 			won_or_lost = random.randint(0,1)
 			if won_or_lost == 1:
 				g_exp = 50
-				exp = await self.update_exp(player_id = ctx.author.id,amount= 50)
+				c_exp, n_exp = await self.update_exp(player_id = ctx.author.id,amount= 50)
 				bal = await self.update_balance(player_id = ctx.author.id,amount = amount, add = True)
 				
-				text = f"\U0001f38a **Congrats** {ctx.author.name},\nThe coin landed on **Tails!** You chose **Tails**, meaning that you've just won **${amount}**!! \n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp**  "
+				c_lvl = self.get_level(c_exp)
+				lvl_up = self.level_up_check(c_exp, n_exp)
+
+				text = f"\U0001f38a **Congrats** {ctx.author.name},\nThe coin landed on **Tails!** You chose **Tails**, meaning that you've just won **${amount}**!! \n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp from this coinflip**  "
+				if lvl_up:
+					text+=f"\n\U0001f389 You levelled up! `({c_lvl} -> {c_lvl+1})` and gained **$1000**"
+					await self.update_balance(player_id = ctx.author.id,amount =  1000, add = True)
 				embed.description = text
 				await msg.edit(embed = embed , view = view)
 
 			elif won_or_lost == 0:
 				g_exp = 25
-				exp = await self.update_exp(player_id = ctx.author.id,amount= 25)
+				c_exp, n_exp = await self.update_exp(player_id = ctx.author.id,amount= 25)
 				bal = await self.update_balance(player_id = ctx.author.id,amount = amount, add =False)
 				
-				text = f"\U0001f626 **Aw snap,** {ctx.author.name},\nThe coin landed on **Heads** You chose **Tails**, meaning that you've just lost **${amount}**!\n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp**  "
+				c_lvl = self.get_level(c_exp)
+				lvl_up = self.level_up_check(c_exp, n_exp)
+
+
+				text = f"\U0001f626 **Aw snap,** {ctx.author.name},\nThe coin landed on **Heads** You chose **Tails**, meaning that you've just lost **${amount}**!\n\nYour new balance is **${bal}**\nYou gained <:exp:896086434946097162>**{g_exp} exp in this coinflip**  "
+				if lvl_up:
+					text+=f"\n\U0001f389 You levelled up! `({c_lvl} -> {c_lvl+1})` and gained **$1000**"
+					await self.update_balance(player_id = ctx.author.id,amount =  1000, add = True)
 				embed.description = text
 				await msg.edit(embed = embed , view = view)
 		else:
