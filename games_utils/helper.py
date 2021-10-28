300681d254a94570b908b823c8dc5697177d6d01
Sherlock-FH <princefuwad@gmail.com>
2021-10-28 20:25:28 +0600

    debugging the bulk update

--- games_utils/helper.py
+++ games_utils/helper.py
@@ -187,8 +187,10 @@ class BattleFieldHelper:
 				except KeyError:
 					t_dict[str(item)] = int(count)
 				t_json = json.dumps(t_dict)
+				print(t_json)
 				query = f"UPDATE inventory SET {rarity_tier} = $1 WHERE p_id = $2;"
-				await self.bot.db.execute(query, t_json, player_id)
+				con = await self.bot.db.execute(query, t_json, player_id)
+				print(con)
 			return True
 		except Exception as e:
 			print(e)
