ca76c5a2cddfa2a39123c72e7db066aba0640fc0
Sherlock-FH <princefuwad@gmail.com>
2021-10-30 18:46:32 +0600

    basic error handler

--- cogs/games.py
+++ cogs/games.py
@@ -21,8 +21,7 @@ class BattleField(commands.Cog):
 		self.bot = bot
 		self.bfh = helper.BattleFieldHelper(bot)
 
-	async def cog_command_error(self, ctx, error):
-		return await ctx.send(error)
+	
 
 
 	@commands.command(name = 'start', aliases = ['enter', 'init'], brief = "Creates an account for playing in Battlefield!", help = "Enter the `Snowden's BattleField` by creating an account!")
