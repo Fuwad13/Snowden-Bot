db21e25a9a1d74c2309d781cb10ee4a7f1d89acf
Sherlock <56512766+Sherlock-FH@users.noreply.github.com>
2021-11-09 21:40:35 +0600

    slash commands debugging

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ class SnowdenBot(commands.AutoShardedBot):
 
 
 bot = SnowdenBot(
-    command_prefix=get_prefix, intents=intents, case_insensitive=True, strip_after_prefix=True,slash_commands = True)
+    command_prefix=get_prefix, intents=intents, case_insensitive=True, strip_after_prefix=True,slash_commands = False)
 
 
 
