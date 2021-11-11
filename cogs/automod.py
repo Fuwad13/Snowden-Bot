38bc6085948d280ddecd112ad53bf7d69c69b335
Sherlock-FH <princefuwad@gmail.com>
2021-11-11 21:02:56 +0600

    bl system test

--- cogs/automod.py
+++ cogs/automod.py
@@ -14,13 +14,13 @@ class AutoModeration(commands.Cog):
 
     @commands.group(name = 'blacklist', aliases = ['bl'], slash_command = False, help = "The group command for blacklisting words for chat automoderation.", invoke_without_command = True)
     @commands.guild_only()
-    @commands.has_permissions(manage_server = True)
+    @commands.has_permissions(manage_guild = True)
     async def _blacklist(self, ctx):
         await ctx.send("Use the subcommands to add/remove blacklisted words")
 
     @_blacklist.command(name= 'add', aliases = ['append'], help = "add new word(s) to the blacklisted words, seperate words by spaces", slash_command = False)
     @commands.guild_only()
-    @commands.has_permissions(manage_server = True)
+    @commands.has_permissions(manage_guild = True)
     async def _bl_add(self, ctx, *, words : str):
         if len(words) < 3:
             return await ctx.send("Words must contain at least 3 characters")
@@ -33,7 +33,7 @@ class AutoModeration(commands.Cog):
         await self.bot.db.execute("Update guilds set bl_words = $1 where guild_id = $2;", j_l, ctx.guild.id)
         r_str = ", ".join(n_l)
         await ctx.send(f"Added these words to the blacklisted words :\n{r_str}")
-        
+
 
 
 def setup(bot):
