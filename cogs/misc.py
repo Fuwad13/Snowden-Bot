6914822dfbd302a6b2a805b80f8d80c260fd27ae
Sherlock-FH <princefuwad@gmail.com>
2021-11-14 20:17:06 +0600

    to unix' :

--- cogs/misc.py
+++ cogs/misc.py
@@ -12,6 +12,8 @@ class Miscellanous(commands.Cog):
 	@commands.command(name = 'tounix', aliases = ['2uinx', 'tu'], slash_command = False, help= "Get unix timestamp from datetime. Datetime example = 2020/11/25 12:56:54" )
 	@commands.cooldown(1, 5, BucketType.user)
 	async def _tounix(self, ctx, *, datetime : str):
+		datetime = datetime.replace('-', '/')
+		datetime = datetime.replace('.', '/')
 		base_url = "https://showcase.api.linx.twenty57.net/UnixTime/tounixtimestamp?datetime="
 		url = base_url+datetime
 		async with self.bot.session.get(url) as resp:
@@ -26,7 +28,7 @@ class Miscellanous(commands.Cog):
 					return await ctx.send(f"**Error**: {err}")
 
 				ts = js['UnixTimeStamp']
-				await ctx.send(f"Unix timestamp : `{ts}`\n<t:{ts}>")
+				await ctx.send(f"Unix timestamp : `{ts}`\n<t:{ts}:t> -> `<t:{ts}:t>`\n<t:{ts}:T> -> `<t:{ts}:T>`\n<t:{ts}> -> `<t:{ts}>` | `<t:{ts}:f>`\n<t:{ts}:F> -> `<t:{ts}:F>`\n<t:{ts}:d> -> `<t:{ts}:d>`\n<t:{ts}:D> -> `<t:{ts}:D>`\n<t:{ts}:R> -> `<t:{ts}:R>`\n")
 
 	
 
