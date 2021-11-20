b88995ddd376cfcfb6f77dad2a207b922bebcf8f
Sherlock-FH <princefuwad@gmail.com>
2021-11-21 00:04:32 +0600

    guild join/leave event

--- cogs/owner.py
+++ cogs/owner.py
@@ -82,12 +82,12 @@ class Owner(commands.Cog):
 		r = await self.bot.db.execute("UPDATE battlefield SET hp = $1 where p_id = $2;", amount, player_id)
 		await ctx.send(str(r))
 
-	@_set.command(name = "bal", aliases = ['bal', 'balance'], hidden = True)
-	@commands.is_owner()
-	async def _setbal(self, ctx, player : typing.Union[discord.Member, discord.User], amount : int):
-		player_id = player.id
-		r = await self.bot.db.execute("UPDATE battlefield SET balance = $1 where p_id = $2;", amount, player_id)
-		await ctx.send(str(r))
+	# @_set.command(name = "bal", aliases = ['bal', 'balance'], hidden = True)
+	# @commands.is_owner()
+	# async def _setbal(self, ctx, player : typing.Union[discord.Member, discord.User], amount : int):
+	# 	player_id = player.id
+	# 	r = await self.bot.db.execute("UPDATE battlefield SET balance = $1 where p_id = $2;", amount, player_id)
+	# 	await ctx.send(str(r))
 
 	@commands.command(name = "/cleanup", aliases = ['/clean'],hidden = True )
 	@commands.is_owner()
