b30b6a83fecafd827cd2dbd71ec7bc6683499d23
Sherlock-FH <princefuwad@gmail.com>
2021-11-20 18:55:07 +0600

    kick command

--- utils/emojis.py
+++ utils/emojis.py
@@ -1,7 +1,7 @@
 EMOJIS = {
     'exp' : '<:exp:896086434946097162>',
     'greentick' : '<:greentick:880695423516430336>',
-    'redtick' : '<:redtick:880695424510476288>',
+    'redtick' : '<:redTick:876471581054996550>',
     'toggle_on' : '<:toggle_on:890713040880795699>',
     'toggle_off' : '<:toggle_off:890712914330271764>'
 }
