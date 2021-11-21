5984d50a3c51ae0204fd4c43e3b4bf18e880f120
Sherlock-FH <princefuwad@gmail.com>
2021-11-21 16:51:24 +0600

    webhook logger

--- cogs/events.py
+++ cogs/events.py
@@ -51,7 +51,7 @@ class Events(commands.Cog):
         channel = self.bot.get_channel(911925727140655134)
         async with aiohttp.ClientSession() as session:
             webhook = discord.Webhook.from_url(url= "https://discord.com/api/webhooks/911927283932082206/KWoFRcttqpkOKAvR7RuCtziMekjR3b-pvSeFnODPRWUdy8MjvFsQUKd4m1Fl5_QWNJ2d", session = session)
-            await webhook.send(message)
+            await webhook.send(message, allowed_mentions= discord.AllowedMentions.none())
 
         
 
