be05ba0db8e6bcd9fb71ddd0da4abc6d5b1d9bba
Sherlock-FH <princefuwad@gmail.com>
2021-11-23 18:14:52 +0600

    weather command

--- cogs/misc.py
+++ cogs/misc.py
@@ -50,6 +50,64 @@ class Miscellanous(commands.Cog):
 				ts = js['Datetime']
 				await ctx.send(f"**{ts}** UTC")
 
+	@commands.command(aliases=["wea"], name = 'weather', brief = 'Get current weather information for a city or location')
+	@commands.cooldown(1, 10, type=commands.BucketType.user)
+	async def weather(self, ctx, *, city: str):
+		city_name = city
+		api_key = "ea5785a913a3474bc8ed46f7d862327f"
+		base_url = "http://api.openweathermap.org/data/2.5/weather?"
+		complete_url = base_url + "appid=" + api_key + "&q=" + city_name+"&units=metric"
+		async with aiohttp.ClientSession() as session:
+			async with session.get(complete_url) as res:
+				x = await res.json()
+		#response = requests.get(complete_url)
+		
+		thermo = u"\U0001f321"
+		hum = u"\U0001f4a6"
+		des = u"\U0001f505"
+		pres = u"\U0001f4cd"
+		wea = u"\U0001f326"
+		feel = u"\U0001f912"
+		sp = u"\U0001f300"
+		channel = ctx.message.channel
+		if x["cod"] != "404":
+			async with channel.typing():
+				y = x["main"]
+				current_temperature_celcius = y["temp"]
+
+				current_pressure = y["pressure"]
+				current_humidity = y["humidity"]
+				feels_like_celcius = y["feels_like"]
+				z = x["weather"]
+				icon_id = z[0]["icon"]
+				icon_url = "http://openweathermap.org/img/wn/" + \
+					str(icon_id)+"@2x.png"
+				wind = x["wind"]
+				speed = wind["speed"]
+				weather_description = z[0]["description"]
+				weather_description = z[0]["description"]
+				c_name = x["name"]
+				embed = discord.Embed(title=f"{wea}Weather in {c_name}",
+									color=0x90FF90,
+									timestamp=ctx.message.created_at,)
+				embed.set_author(icon_url=icon_url, name="Weather Updates")
+				embed.add_field(
+					name=f"{des}Descripition", value=f"**{weather_description}**", inline=False)
+				embed.add_field(
+					name=f"{thermo}Temperature(C)", value=f"**{current_temperature_celcius}°C**", inline=True)
+				embed.add_field(
+					name=f"{feel}Feels like(C)", value=f"**{feels_like_celcius}°C**", inline=True)
+				embed.add_field(
+					name=f"{hum}Humidity(%)", value=f"**{current_humidity}%**", inline=True)
+				embed.add_field(
+					name=f"{sp}Wind speed(m/s)", value=f"**{speed} meter/second**", inline=True)
+				embed.add_field(name=f"{pres}Atmospheric Pressure(hPa)",
+								value=f"**{current_pressure}hPa**", inline=True)
+				embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+				embed.set_footer(text=f"Requested by {ctx.author.name}")
+				await ctx.reply(embed=embed, mention_author=False)
+		else:
+			await ctx.send("City not found.", ephemeral = True)
 	
 
 	
