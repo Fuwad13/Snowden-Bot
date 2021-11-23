455da691d9b1bb5746111d618e08f7cb8814de42
Sherlock-FH <princefuwad@gmail.com>
2021-11-23 23:52:23 +0600

    purge subcommands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -102,6 +102,41 @@ class Moderation(commands.Cog):
         deleted = await ctx.channel.purge(limit= amount, before = ctx.message.created_at, check = is_embed)
         await ctx.send(f"**Deleted {len(deleted)} message(s) containing embeds**" , delete_after = 5)
 
+    @_purge.command(name= "bots", aliases = ['robots', 'bot'], help = "Bulk delete messages that were sent by bots.", slash_command = False)
+    @commands.has_permissions(manage_messages= True)
+    @commands.bot_has_permissions(manage_messages = True)
+    async def _bots(self, ctx, amount : int = 5):
+        if amount > 1000:
+            return await ctx.send("**Can't delete more than 1000 messages at once!**")
+
+        def is_bot(message):
+            if not (time.time() - message.created_at.timestamp()) < 1209600:
+                return False
+            if message.author.bot:
+                return True
+            else: 
+                return False
+        deleted = await ctx.channel.purge(limit= amount, before = ctx.message.created_at, check = is_bot)
+        await ctx.send(f"**Deleted {len(deleted)} message(s) that were sent by bots.**" , delete_after = 7)
+
+    @_purge.command(name= "texts", aliases = ['text'], help = "Bulk delete messages that contains only texts and not any embed/files.", slash_command = False)
+    @commands.has_permissions(manage_messages= True)
+    @commands.bot_has_permissions(manage_messages = True)
+    async def _texts(self, ctx, amount : int = 5):
+        if amount > 1000:
+            return await ctx.send("**Can't delete more than 1000 messages at once!**")
+
+        
+        def is_text(message):
+            if not (time.time() - message.created_at.timestamp()) < 1209600:
+                return False
+            if len(message.content) > 0 and len(message.embeds) == 0 and len(message.attachments) == 0:
+                return True
+            else: 
+                return False
+        deleted = await ctx.channel.purge(limit= amount, before = ctx.message.created_at, check = is_text)
+        await ctx.send(f"**Deleted {len(deleted)} message(s) containing only texts**" , delete_after = 5)
+
     @commands.command(name= 'kick', brief = "Kick a member for breaking rules or being annoying.", help = "Kick a member from your server for breaking rules or being an annoying person.")
     @commands.has_permissions(kick_members = True)
     @commands.bot_has_permissions(kick_members = True)
